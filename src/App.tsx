0437954d7fdb54937256da6d2a8751911506fda9
Carlos Marx <carlosmarx27@gmail.com>
2022-06-24 16:03:17 -0300

    Add page subscriber

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import {gql, useQuery} from "@apollo/client";
+import {ApolloProvider} from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { client } from './lib/apollo'
 import { Event } from "./pages/Event";
-
+import { Router } from "./Router";
 
 function App() {
 
  
   return (
     <>
-      <Event />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
     </>
   )
 }
